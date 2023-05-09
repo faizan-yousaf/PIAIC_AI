ca23f76b472b34a57717bf86b4bcf76d8010ec08
Muhammad Faizan <faizanyousaf815@gmail.com>
2023-05-09 23:53:47 +0500

    added piaic

---
+++ piaic/maths.py
@@ -0,0 +1,8 @@
+def f1():
+    return "This is my first pakage in python!"
+def f2():
+    return "This is my first pakage in python!"
+def f3():
+    return "This is my first pakage in python!"
+def f4():
+    return "This is my first pakage in python!"
